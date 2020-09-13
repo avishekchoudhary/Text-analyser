6f0475cb2d1540d43301af6dad055c8962d9ed52
Avishek Choudhary <56909326+avishekchoudhary@users.noreply.github.com>
2020-09-13 17:31:06 +0530

    Update settings.py

--- text_analyser/settings.py
+++ text_analyser/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lm8k&ktgx(!v@k&%p2xdpwmw9*bzb7(ie5iv!w#2ru^o41j70='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
